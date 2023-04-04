b338f3b7849e021a72e2f5e7a9c1951f7fc406e1
edumatheus65 <ematheusneves13@gmail.com>
2023-04-04 08:57:35 -0300

    created connection directory with the firestore database

---
+++ miniblog/firebase/config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firebase'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA0xrtiFzNhMMLJPfmeTx7FCFeo7QSigE0",
+  authDomain: "miniblog-ff36c.firebaseapp.com",
+  projectId: "miniblog-ff36c",
+  storageBucket: "miniblog-ff36c.appspot.com",
+  messagingSenderId: "422455984951",
+  appId: "1:422455984951:web:35f54f76b2486fd7e6d0b2"
+};
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app)
+
+export { db };
