870b61cabdc50179b8609dc71d52b0209c507759
edumatheus65 <ematheusneves13@gmail.com>
2023-05-15 09:57:23 -0300

    Creating edit page

--- miniblog/src/pages/Dashboard/Dashboard.js
+++ miniblog/src/pages/Dashboard/Dashboard.js
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom"
 // hooks
 import { useAuthValue } from "../../contexts/AuthContext"
 import { useFetchDocuments } from "../../hooks/useFetchDocuments"
+import { useDeleteDocument } from "../../hooks/useDeleteDocument"
 
 // components
 import PostDetail from "../../components/PostDetail";
@@ -14,10 +15,7 @@ const Dashboard = () => {
   const uid = user.uid
 
   const { documents: posts, loading } = useFetchDocuments("posts", null, uid)
-
-  const deleteDocument = (id) => {
-
-  }
+  const { deleteDocument } = useDeleteDocument("posts")  
 
   if(loading) {
     return <p>Carregando...</p>
---
+++ miniblog/src/pages/EditPost/EditPost.js
@@ -0,0 +1,130 @@
+import { useState, useEffect } from "react"
+import { useNavigate, useParams } from "react-router-dom"
+import { useAuthValue } from "../../contexts/AuthContext"
+import styles from "./EditPost.module.css"
+import { useInsertDocument } from "../../hooks/useInsertDocument"
+import { useFetchDocument } from "../../hooks/useFetchDocument"
+
+const EditPost = () => {
+    const { id } = useParams()
+    const { document: post } = useFetchDocument("posts", id)
+
+    const [title, setTitle] = useState("")
+    const [image, setImage] = useState("")
+    const [body, setBody] = useState("")
+    const [tags, setTags] = useState([])
+    const [formError, setFormError] = useState("")
+
+
+    useEffect(() => {
+        if (post) {
+            setTitle(post.title)
+            setBody(post.body)
+            setImage(post.image)
+
+            const textTags = post.tags.join(", ")
+
+            setTags(textTags)
+        }
+    }, [post])
+
+    const { user } = useAuthValue();
+    const { insertDocument, response } = useInsertDocument("posts")
+    const navigate = useNavigate()
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setFormError("")
+
+        // validate image URL
+        try {
+            new URL(image)
+        } catch (error) {
+            return setFormError("A imagem precisa ser uma URL")
+        }
+
+        // criar o array de tags
+        const tagsArray = tags.split(",").map((tag) => tag.trim().toLowerCase());
+
+        // checar todos os valores
+
+        if (!title || !image || !body || !tags) {
+            setFormError("Por favor, preencha todos os campos.")
+        }
+
+        if (formError) return;
+
+        insertDocument({
+            title,
+            image,
+            body,
+            tags: tagsArray,
+            uid: user.uid,
+            createdBy: user.displayName,
+        });
+
+        // redirect to home page
+        navigate("/")
+    };
+
+    return (
+        <div className={styles.edit_post}>
+            {post && (
+                <>
+                    <h2>Editar Post: {post.title}</h2>
+                    <p>Altere aqui seu post</p>
+                    <form onSubmit={handleSubmit}>
+                        <label>
+                            <span>Título:</span>
+                            <input type="text"
+                                name="title"
+                                required
+                                placeholder="Escreva o título..."
+                                onChange={(e) => setTitle(e.target.value)}
+                                value={title} />
+                        </label>
+                        <label>
+                            <span>URL da imagem:</span>
+                            <input type="text"
+                                name="image"
+                                required
+                                placeholder="Escolha uma boa imagem..."
+                                onChange={(e) => setImage(e.target.value)}
+                                value={image} />
+                        </label>
+                        <label>
+                            <p className={styles.preview_title}>Preview da imagem atual:</p>
+                            <img className={styles.image_preview}
+                                src={post.image}
+                                alt={post.title}
+                            />
+                            <span>conteudo:</span>
+                            <textarea name="body"
+                                required
+                                placeholder="Escreva seu conteudo do seu post"
+                                onChange={(e) => setBody(e.target.value)}
+                                value={body}></textarea>
+                        </label>
+                        <label>
+                            <span>Tags:</span>
+                            <input type="text"
+                                name="tags"
+                                required
+                                placeholder="Escolha as tags separadas por vírgula"
+                                onChange={(e) => setTags(e.target.value)}
+                                value={tags} />
+                        </label>
+                        {!response.loading && <button className="btn">Editar</button>}
+                        {response.loading && (
+                            <button className="btn" disabled>Aguarde...</button>
+                        )}
+                        {response.error && <p className="error">{response.error}</p>}
+                        {formError && <p className="error">{formError}</p>}
+                    </form>
+                </>
+            )}
+        </div>
+    )
+}
+
+export default EditPost
